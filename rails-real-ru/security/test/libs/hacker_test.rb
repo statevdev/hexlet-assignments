929359cb14bc4953936db1ff72395e7614797484
@hexlet/cli <support@hexlet.io>
2025-03-25 19:49:23 +0600

    submit rails-real-ru/security

---
+++ rails-real-ru/security/test/libs/hacker_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'rake'
+
+class HackerTest < ActiveSupport::TestCase
+  def setup
+    @authenticity_token = 'auth-token'
+    @cookie = 'cookie-test'
+    @uri = 'https://rails-collective-blog-ru.hexlet.app'
+  end
+
+  test 'test task' do
+    body =
+      <<~HTML
+        <html>
+          <meta name="csrf-token" content='#{@authenticity_token}'>
+          <input type='hidden' name='authenticity_token' value='#{@authenticity_token}'>
+        </html>
+      HTML
+
+    stub_request(:get, "#{@uri}/users/sign_up")
+      .to_return(
+        body:,
+        headers: { 'Set-Cookie': @cookie }
+      )
+
+    stub_request(:post, "#{@uri}/users")
+      .with(
+        body: hash_including({ authenticity_token: @authenticity_token }),
+        headers: { cookie: @cookie }
+      )
+      .to_return(status: 302)
+
+    response = Hacker.hack('test@test.io', 'password')
+
+    assert response
+  end
+end
